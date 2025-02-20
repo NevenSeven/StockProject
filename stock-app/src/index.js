db936ee4c26fa981b4fa87271ea25f6220652800
Neven <ncz24>
2025-02-20 16:49:50 -0500

    Updated index.js to add api key

--- stock-app/src/index.js
+++ stock-app/src/index.js
@@ -11,6 +11,35 @@ root.render(
   </React.StrictMode>
 );
 
+//define the url to get data from
+const apiKey = 'dLwemTM9r8JjXR8o7082T2tsNYcQGz9i'
+const apiUrl = `https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-02-10?adjusted=true&sort=asc&apiKey=${apiKey}`;
+
+const requestOptions = {
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${apiKey}`,
+  },
+};
+
+const outputElement = document.getElementById('output')
+
+//make a request to get the data
+fetch(apiUrl, requestOptions)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    //display data in HTML element
+    outputElement.textContent = JSON.stringify(data, null, 2);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
