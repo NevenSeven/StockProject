815a8351e77f1447878ffc316a232370c6952843
Riley Robbins <rjr337@drexel.edu>
2025-04-08 18:00:20 -0400

    Burger work now yes!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,11 +9,11 @@ const Navbar = () => {
         <Link className="navbar-brand" to="/">
           <img src="/imgs/logo_alt.png" alt="StockStalker" width="225px" />
         </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
             </li>
