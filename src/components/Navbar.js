bf57ed72865d23d7ac95be56ec873c39395e582c
Riley Robbins <rjr337@drexel.edu>
2025-04-08 17:40:54 -0400

    Burger for mobile navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,23 +9,28 @@ const Navbar = () => {
         <Link className="navbar-brand" to="/">
           <img src="/imgs/logo_alt.png" alt="StockStalker" width="225px" />
         </Link>
-        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link className="nav-link" to="/watchlist">Watchlist</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/infostock">Learn About Stocks</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/otherstocks">Other Stocks</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/simulator">Simulator</Link>
-          </li>
-        </ul>
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/watchlist">Watchlist</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/infostock">Learn About Stocks</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/otherstocks">Other Stocks</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/simulator">Simulator</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
