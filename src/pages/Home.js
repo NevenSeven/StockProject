5143707d47345f4ef24a6b4ffd2071ab8c953116
Soham Deshmukh <ssd333@drexel.edu>
2025-04-10 15:51:32 -0400

    chart items

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Stockdata from '../components/Stockdata'
+import Chart from '../components/Chart'
 
 const Home = () => {
     return (
       <>
         <Stockdata />
+        <Chart />
       </>
     );
   };
