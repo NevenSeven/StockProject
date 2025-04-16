6fb6eb2419d88b8d39039c1b13f2e25bceee79ef
Neven Zurcher <ncz24@drexel.edu>
2025-04-16 18:24:17 -0400

    removed popup from layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import PopupSpawner from './Popup' 
 
 import { Outlet } from 'react-router-dom';
 
@@ -9,7 +8,6 @@ const Layout = () => {
   return (
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <PopupSpawner />
       <main className="flex-fill">
         <Outlet />
       </main>
