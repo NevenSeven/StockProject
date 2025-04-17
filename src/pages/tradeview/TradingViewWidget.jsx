6c7a7cd22044723272032fe347907a6350a65cdf
Neven Zurcher <ncz24@drexel.edu>
2025-04-16 21:23:08 -0400

    updated tradingviewwidget

--- src/pages/tradeview/TradingViewWidget.jsx
+++ src/pages/tradeview/TradingViewWidget.jsx
@@ -16,11 +16,10 @@ function TradingViewWidget() {
           "symbol": "NASDAQ:AAPL",
           "interval": "D",
           "timezone": "Etc/UTC",
-          "theme": "dark",
+          "theme": "light",
           "style": "1",
           "locale": "en",
           "allow_symbol_change": true,
-          "support_host": "https://www.tradingview.com"
         }`;
       container.current.appendChild(script);
     },
@@ -30,7 +29,6 @@ function TradingViewWidget() {
   return (
     <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
       <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
-      <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a></div>
     </div>
   );
 }
