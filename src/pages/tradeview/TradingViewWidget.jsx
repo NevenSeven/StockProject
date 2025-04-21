b2c6c520c034a5d338ba25c712e181c6651838dc
Riley Robbins <rjr337@drexel.edu>
2025-04-21 18:18:19 -0400

    chart work 3

--- src/pages/tradeview/TradingViewWidget.jsx
+++ src/pages/tradeview/TradingViewWidget.jsx
@@ -1,75 +1,61 @@
 // TradingViewWidget.jsx
-import React, { useEffect, useRef, memo } from 'react';
+import React, { useEffect, useRef } from "react";
 
 function TradingViewWidget() {
-  const container = useRef();
+  const containerRef = useRef();
 
-  useEffect(
-    () => {
-      const script = document.createElement("script");
-      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js";
-      script.type = "text/javascript";
-      script.async = true;
-      script.textContent  = `
-        {
-          "symbols": [
-            [
-              "Apple",
-              "AAPL|1D"
-            ],
-            [
-              "Google",
-              "GOOGL|1D"
-            ],
-            [
-              "Microsoft",
-              "MSFT|1D"
-            ]
-          ],
-          "chartOnly": false,
-          "width": "100%",
-          "height": "100%",
-          "locale": "en",
-          "colorTheme": "light",
-          "autosize": false,
-          "showVolume": false,
-          "showMA": false,
-          "hideDateRanges": false,
-          "hideMarketStatus": false,
-          "hideSymbolLogo": false,
-          "scalePosition": "right",
-          "scaleMode": "Normal",
-          "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
-          "fontSize": "10",
-          "noTimeScale": false,
-          "valuesTracking": "1",
-          "changeMode": "price-and-percent",
-          "chartType": "area",
-          "maLineColor": "#2962FF",
-          "maLineWidth": 1,
-          "maLength": 9,
-          "headerFontSize": "medium",
-          "lineWidth": 2,
-          "lineType": 0,
-          "dateRanges": [
-            "1d|1",
-            "1m|30",
-            "3m|60",
-            "12m|1D",
-            "60m|1W",
-            "all|1M"
-          ]
-        }`;
-      container.current.appendChild(script);
-    },
-    []
-  );
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = JSON.stringify({
+      symbols: [
+        ["Apple", "AAPL|1D"],
+        ["Google", "GOOGL|1D"],
+        ["Microsoft", "MSFT|1D"]
+      ],
+      chartOnly: false,
+      width: "100%",
+      height: "100%",
+      locale: "en",
+      colorTheme: "light",
+      autosize: true,
+      showVolume: false,
+      showMA: false,
+      hideDateRanges: false,
+      hideMarketStatus: false,
+      hideSymbolLogo: false,
+      scalePosition: "right",
+      scaleMode: "Normal",
+      fontFamily: "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+      fontSize: "10",
+      noTimeScale: false,
+      valuesTracking: "1",
+      changeMode: "price-and-percent",
+      chartType: "area",
+      maLineColor: "#2962FF",
+      maLineWidth: 1,
+      maLength: 9,
+      headerFontSize: "medium",
+      lineWidth: 2,
+      lineType: 0,
+      dateRanges: [
+        "1d|1", "1m|30", "3m|60", "12m|1D", "60m|1W", "all|1M"
+      ]
+    });
+    containerRef.current.appendChild(script);
+  }, []);
 
   return (
-    <div className="tradingview-widget-container" ref={container} style={{ height: "450px", width: "100%" }}>
-      <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
+    <div
+      className="tradingview-widget-container"
+      ref={containerRef}
+      style={{ height: "500px", width: "100%" }}
+    >
+      <div className="tradingview-widget-container__widget" />
     </div>
   );
 }
 
-export default memo(TradingViewWidget);
+export default TradingViewWidget;
